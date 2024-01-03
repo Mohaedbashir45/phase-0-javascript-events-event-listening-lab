32ed2cb0333915a9daa3f07a077d0b3dde9a1a0e
mohamedbashir <mohaedbashir45@gmail.com>
2024-01-03 17:47:01 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+  const input = document.getElementById('input');
+  if (input) {
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+  } else {
+    console.error('Input element not found');
+  }
 }
+addingEventListener();
